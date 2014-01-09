57b2068e9bc6e1fd54c34119359c6c550938f651
jas- <jason.gerfen@gmail.com>
2014-01-09 12:16:04 -0700

    scan: In progress save Thu Jan  9 12:16:04 MST 2014
    methods:
     scan(opts): Working on async scan method for user defined host
                 scanning.
    
    global:
     Moved init of child_process, async & cluster module into global
     scope.
    
    setup:
     verify(opts): Added to handle user defined options and proper
                 format for library.

--- test/run.js
+++ test/run.js
@@ -1,6 +1,34 @@
-require('../').nmap('discover', {
+var libnmap = require('../')
+
+console.log('node-libnmap module details')
+console.log('')
+console.log(libnmap.nmap())
+console.log('')
+
+console.log('Performing auto-discovery of nearby hosts')
+console.log('')
+
+libnmap.nmap('discover', {
   callback: function(err, report){
     if (err) throw err
+
     console.log(report)
+    secondaryScan(report)
   }
 })
+
+function secondaryScan(report){
+  report.forEach(function(details){
+
+    libnmap.nmap('scan', {
+      flags: '-T4 -A -oG -',
+      range: details.neighbors,
+      callback: function(err, report){
+        if (err) throw err
+
+        console.log('')
+        console.log(report)
+      }
+    })
+  })
+}
