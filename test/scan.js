ad3356e54262af4d3c4caa3a27c2e3dfa9be0f86
jas- <jason.gerfen@gmail.com>
2014-01-12 06:09:23 -0700

    Tests & Options
    test:
     scan.js: Removed the -A flag from scan to speed up test case
    
    options:
     verify: No performs proper regex on port specifications

--- test/scan.js
+++ test/scan.js
@@ -4,10 +4,12 @@ console.log('')
 console.log('Testing single, CIDR & range notation scans')
 console.log('')
 libnmap.nmap('scan', {
-  flags: '-T4 -A -oG -',
-  range: ['10.0.2.15', '10.0.2.0/24', '10.0.2.0-25'],
+  flags: '-T4 -oG -',
+  range: ['10.0.2.0/24'],
+  ports: '1-1024',
   callback: function(err, report){
     if (err) throw err
+
     console.log(report)
   }
 })
