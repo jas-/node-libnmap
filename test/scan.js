0b84032bffb8d4c759124680bfe5523f718ba8a5
jas- <jason.gerfen@gmail.com>
2014-01-12 17:08:32 -0700

    v0.0.3-scan branch complete

--- test/scan.js
+++ test/scan.js
@@ -5,8 +5,8 @@ console.log('Testing single, CIDR & range notation scans')
 console.log('')
 libnmap.nmap('scan', {
   flags: '-T4 -oG -',
-  range: ['10.0.2.0/24'],
-  ports: '1-1024',
+  range: ['10.0.2.15', '10.0.2.0/24', '10.0.2.0-16'],
+  ports: 1-1024,
   callback: function(err, report){
     if (err) throw err
 
