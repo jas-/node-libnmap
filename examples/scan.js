0684d291b178839c5a73d2a7560676199f99d5ed
Jas- <jason.gerfen@gmail.com>
2015-08-31 00:52:40 +0000

    Removed duplicate reports from being generated per host

--- examples/scan.js
+++ examples/scan.js
@@ -6,14 +6,30 @@
 
 var nmap = require('../')
   , opts = {
+      //range: ['scanme.nmap.org', 'localhost', '172.17.190.0/16'],
       range: ['scanme.nmap.org', 'localhost'],
       //range: ['localhost', 'scanme.nmap.org', '10.0.2.0/16', '192.168.1.10-100'],
-      ports: '21,22,80,443,2000-3000,8080,8443'
+      //ports: '21,22,80,443,2000-3000,8080,8443'
+      ports: '22,80,443'
     };
 
+function printer(obj) {
+  if (/object|array/.test(obj)) {
+    for (var key in obj) {
+      console.log(key);
+      if (/object|array/.test(obj[key])) {
+        printer(obj[key]);
+      } else {
+        console.log('   '+obj[key]);
+      }
+    }
+  }
+}
 
 nmap.scan(opts, function(err, report) {
   if (err) throw new Error(err);
 
-  console.log(report);
+  console.log(Object.keys(report).length);
+  console.log(report['scanme.nmap.org']);
+  console.log(report['localhost']);
 });
