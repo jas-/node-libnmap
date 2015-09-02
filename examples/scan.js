d362875bb2e4196191130becfcab3d0e547dcc8b
Jas- <jason.gerfen@gmail.com>
2015-09-02 02:51:29 +0000

    Minor updates

--- examples/scan.js
+++ examples/scan.js
@@ -8,8 +8,10 @@ var nmap = require('../')
   , fs = require('fs')
   , path = './scans/'
   , opts = {
-      range: ['scanme.nmap.org', 'localhost', '172.17.190.0/16'],
-      ports: '21,22,80,443,2000-3000,8080,8443'
+      //range: ['scanme.nmap.org', 'localhost', '172.17.190.0/16'],
+      range: ['scanme.nmap.org', 'localhost'],
+      //ports: '21,22,80,443,2000-3000,8080,8443'
+      ports: '21,22,23,80,443'
     };
 
 nmap.scan(opts, function(err, report) {
@@ -17,7 +19,6 @@ nmap.scan(opts, function(err, report) {
 
   for (var item in report) {
     for (var host in report[item].host) {
-      console.log(report[item].host[host])
 
       var data = JSON.stringify(report[item].host[host])
         , filename = report[item].host[host].address.addr;
