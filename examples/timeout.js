67c3a5534ff09f72da8f81abf5b203f929a80f9f
Jas- <jason.gerfen@gmail.com>
2015-09-09 02:09:37 +0000

    Added timeout example

---
+++ examples/timeout.js
@@ -0,0 +1,19 @@
+/*!
+ * node-libnmap
+ * Copyright(c) 2013-2015 Jason Gerfen <jason.gerfen@gmail.com>
+ * License: MIT
+ */
+
+var nmap = require('node-nmap')
+  , opts = {
+      timeout: 900, // 900s = 10m and increases the reliability of scan results
+      range: ['scanme.nmap.org', '192.168.0.0/17']
+    };
+
+nmap.scan(opts, function(err, report) {
+  if (err) throw new Error(err);
+
+  for (var item in report) {
+    console.log(report[item]);
+  }
+});
