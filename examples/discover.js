8c0033474087210e1afef61586ca3c552ec16a62
Jas- <jason.gerfen@gmail.com>
2015-09-12 15:49:08 +0000

    New example for handling discovery method

---
+++ examples/discover.js
@@ -0,0 +1,15 @@
+/*!
+ * node-libnmap
+ * Copyright(c) 2013-2015 Jason Gerfen <jason.gerfen@gmail.com>
+ * License: MIT
+ */
+
+var nmap = require('../');
+
+nmap.discover(function(err, report) {
+  if (err) throw new Error(err);
+
+  for (var item in report) {
+    console.log(JSON.stringify(report[item]));
+  }
+});
