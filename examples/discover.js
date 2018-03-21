e4b18caca0dae50efb082d0b32c9e3245cd7352e
Jas- <jason.gerfen@gmail.com>
2018-03-21 08:32:05 +0000

    Attempts to fix nvm version for travis-ci in linux/osx; part five

--- examples/discover.js
+++ examples/discover.js
@@ -8,7 +8,7 @@
 
 const nmap = require('../');
 
-nmap.discover({debug: true}, function(err, report) {
+nmap.discover({verbose: true}, function(err, report) {
   if (err) throw new Error(err);
 
   for (let item in report) {
