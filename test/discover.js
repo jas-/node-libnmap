6e797385f80d20fcba1771a49d1519f002647afd
Jas- <jason.gerfen@gmail.com>
2018-09-16 14:54:35 +0000

    v0.4.14: Fix for .match()

--- test/discover.js
+++ test/discover.js
@@ -7,20 +7,20 @@
 'use strict'
 
 const nmap = require('../');
-const timeout = 1024 * 1024;
+const timeout = 1024 * 1024 * 3;
 const chai = require('chai');
 const should = chai.should();
 const expect = chai.expect;
 const ifaces = require('os').networkInterfaces();
 
-describe('nmap', function() {
-  context('discovery method', function() {
+describe('nmap', function () {
+  context('discovery method', function () {
 
     this.timeout(timeout);
 
-    it('validate report', function(done) {
+    it('validate report', function (done) {
 
-      nmap.discover(function(err, report) {
+      nmap.discover(function (err, report) {
 
         /* If 'subnet' doesn't exist in os.networkInterfaces() expect errors */
         for (let adapter in ifaces) {
@@ -28,14 +28,17 @@ describe('nmap', function() {
             if (!ifaces[adapter][0].hasOwnProperty('subnet')) {
               try {
                 done();
-              } catch(error) {
+              }
+              catch (error) {
                 done(error);
               }
-            } else {
+            }
+            else {
               try {
                 should.not.exist(err);
                 should.exist(report);
-              } catch(error) {
+              }
+              catch (error) {
                 done(error);
               }
             }
--- test/scan.js
+++ test/scan.js
@@ -7,7 +7,7 @@
 'use strict'
 
 const nmap = require('../');
-const timeout = 1024 * 1024;
+const timeout = 1024 * 1024 * 3;
 const chai = require('chai');
 const should = chai.should();
 const expect = chai.expect;
@@ -21,13 +21,13 @@ const opts = {
 };
 
 
-describe('scan method', function() {
+describe('scan method', function () {
 
-  context('reporting', function() {
-    it('json', function(done) {
+  context('reporting', function () {
+    it('json', function (done) {
       this.timeout(timeout);
 
-      nmap.scan(opts, function(err, report) {
+      nmap.scan(opts, function (err, report) {
         should.not.exist(err);
 
         report.should.be.a('object');
@@ -35,11 +35,11 @@ describe('scan method', function() {
       });
     });
 
-    it('xml', function(done) {
+    it('xml', function (done) {
       this.timeout(timeout);
       opts.json = false;
 
-      nmap.scan(opts, function(err, report) {
+      nmap.scan(opts, function (err, report) {
         should.not.exist(err);
 
         report.should.be.a('object');
