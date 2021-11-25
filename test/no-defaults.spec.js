de61d8daf07b29046d29d56b771ed852f479bf3e
Evandro <evandroaribel@gmail.com>
2021-11-25 12:06:00 +0100

    Added test and example
    In the tests I had to add a command to run the specific test "no-defaults.spec.js" because I have some kind of error in "discover"

---
+++ test/no-defaults.spec.js
@@ -0,0 +1,50 @@
+/*!
+ * libnmap
+ * Copyright(c) 2013-2019 Jason Gerfen <jason.gerfen@gmail.com>
+ * License: MIT
+ */
+
+'use strict'
+
+const nmap = require('../');
+const timeout = 1024 * 1024 * 3;
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const opts = {
+  range: [
+    '127.0.0.1',
+    'scanme.nmap.org',
+    '::1'
+  ],
+  noDefaults: true
+};
+
+
+describe('scan method', function () {
+
+  context('reporting', function () {
+    it('json', function (done) {
+      this.timeout(timeout);
+
+      nmap.scan(opts, function (err, report) {
+        should.not.exist(err);
+
+        report.should.be.a('object');
+        done();
+      });
+    });
+
+    it('xml', function (done) {
+      this.timeout(timeout);
+      opts.json = false;
+
+      nmap.scan(opts, function (err, report) {
+        should.not.exist(err);
+
+        report.should.be.a('object');
+        done();
+      });
+    });
+  });
+});
